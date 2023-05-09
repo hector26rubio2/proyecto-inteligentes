9dd984b1e00e25b173041dccfaedb32e5e1aad85
hector26rubio2 <hector26rubio@gmail.com>
2023-05-08 21:25:31 -0500

    Revert "Update my_routes.py"
    This reverts commit fb28780ae4a91bc8488c38f1473d120533ea62a0.

--- routes/my_routes.py
+++ routes/my_routes.py
@@ -1,21 +1,18 @@
+
 import datetime
 import os
 from flask import Blueprint, jsonify, request
 
-my_routes = Blueprint('my_routes', __name__)
 
+my_routes = Blueprint('my_routes', __name__)
 
 @my_routes.route('/upload', methods=['POST'])
 def upload_file():
-    try:
-        file = request.files['file']
-        if file:
-            filename = datetime.now().strftime(
-                "%Y-%m-%d_%H-%M-%S") + '_' + file.filename
-            file.save(os.path.join('Archivos/', filename))
-            return jsonify({'message': 'Archivo guardado exitosamente.'}), 200
-        else:
-            return jsonify({'error': 'No se recibió ningún archivo.'}), 400
-    except:
-        return jsonify({'error': 'internal erro.'}), 500
-    
+    file = request.files['file']
+    if file:
+        filename = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + '_' + file.filename
+        file.save(os.path.join('Archivos/', filename))
+        return jsonify({'message': 'Archivo guardado exitosamente.'}), 200
+    else:
+        return jsonify({'error': 'No se recibió ningún archivo.'}), 400
+
